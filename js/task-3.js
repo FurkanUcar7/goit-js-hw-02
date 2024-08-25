d7f2a3a4c04b85abad67a85ed2a3b12375a0b121
Furkan Ucar <f.ucar741@gmail.com>
2024-08-25 15:43:55 +0300

    final

---
+++ js/task-3.js
@@ -0,0 +1,17 @@
+function checkForSpam(message){
+    let spamWord = "sale";
+    let spamWordTwo = "spam";
+    let messageLower = message.toLowerCase();
+    return messageLower.includes(spamWord) || messageLower.includes(spamWordTwo)
+}
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+
+
+// bunu if else ile yapmıştım ilk başta, fakat sonra karşılaştırma operatörlerinin true false döndüğü aklıma geldi, direkt return ile döndürerek kodu sadeleştirdim.
